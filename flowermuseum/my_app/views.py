6305d4b08a8f035375e56d0026dc2698d222de61
Kaya Woods <kayanevada@gmail.com>
2025-03-31 13:43:10 -0700

    added template information

--- flowermuseum/my_app/views.py
+++ flowermuseum/my_app/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
-from django.http import HttpResponse
 
-def home(reqest): 
-    return HttpResponse('<h1>Flower Museum ◟(๑• ᴗ •)◞❁</h1>')
+class Flower: 
+     def __init__(self, chosen_name, type, scent, description):
+          self.chosen_name = chosen_name 
+          self.type = type
+          self.scent = scent 
+          self.description = description 
+flowers = [ 
+     Flower('Marcus', 'Sunflower', 'Floral Butter', 'lush'), 
+     Flower('Hannah', 'Rose', 'Romance', 'thorny'),
+     Flower('Lila', 'Daisy', 'Skunk', 'vibrant af')
+]
+
+def flower_index(request): 
+     return render(request, 'flowers/index.html', {'flowers': flowers})
+
+def home(request): 
+    return render(request,'home.html')
 
 def about(request):
      return render(request, 'about.html')
