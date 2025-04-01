8b6c60e00bbceabd0ccc8f2f1da34b2351c31d30
Kaya Woods <kayanevada@gmail.com>
2025-04-01 14:50:37 -0700

    fixed images

--- flowermuseum/my_app/models.py
+++ flowermuseum/my_app/models.py
@@ -5,6 +5,7 @@ class Flower(models.Model):
     type = models.CharField(max_length=100)
     scent = models.CharField(max_length =150)
     description = models.TextField(max_length=250)
+    image = models.TextField(max_length=250)
 
     def __str__(self):
         return self.chosen_name
