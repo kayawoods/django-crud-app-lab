c7c23068fe14426b20385b926a6f641ffe1d29e7
Kaya Woods <kayanevada@gmail.com>
2025-04-03 10:41:31 -0700

    added watering functionality

--- flowermuseum/my_app/admin.py
+++ flowermuseum/my_app/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Flower 
+from .models import Flower, Watering
 
 admin.site.register(Flower)
-# Register your models here.
+admin.site.register(Watering)
+
--- flowermuseum/my_app/models.py
+++ flowermuseum/my_app/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
+AMOUNT = ( 
+    ('Cup', '8oz'), 
+    ('Pitcher', '32oz'), 
+    ('Watering Can', '64oz')
+)
+
 class Flower(models.Model):
     chosen_name = models.CharField(max_length=100)
     type = models.CharField(max_length=100)
@@ -10,9 +16,18 @@ class Flower(models.Model):
 
     def __str__(self):
         return self.chosen_name
- 
+
     def get_absolute_url(self): 
         return reverse('flower-detail', kwargs={'flower_id': self.id})
     
-    
+class Watering(models.Model): 
+    date = models.DateField('Watering Date')
+    amount = models.CharField(
+        max_length=25,
+        choices=AMOUNT,
+        default=AMOUNT[0][0]
+    )                       
+    flower = models.ForeignKey(Flower, on_delete=models.CASCADE)
 
+def __str__(self): 
+    return f"{self.get_amount_display()} on {self.date}"
